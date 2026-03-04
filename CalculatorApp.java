698c537503d35c59d58fc420b0df636cbaae2063
Aditya Sengar <adityasengar1902@gmail.com>
2026-03-04 14:05:52 +0530

    Refactor Calculator methods and main logic
    Refactored Calculator methods to use integer parameters and updated main method to handle input and output correctly.

--- CalculatorApp.java
+++ CalculatorApp.java
@@ -1,120 +1,36 @@
-// import java.util.Scanner;
-
-// class Calculator {
-//     int add(int a, int b) {
-//         return a + b;
-//     }
-
-//     int sub(int a, int b) {
-//         return a - b;
-//     }
-
-//     int mul(int a, int b) {
-//         return a * b;
-//     }
-
-//     int div(int a, int b) {
-//         if (b == 0) {
-//             System.out.println("Division by zero not allowed");
-//             return 0;
-//         }
-//         return a / b;
-//     }
-
-//     int mod(int a, int b) {
-//         if (b == 0) {
-//             System.out.println("Modulus by zero not allowed");
-//             return 0;
-//         }
-//         return a % b;
-//     }
-// }
-
-// public class CalculatorApp {
-//     public static void main(String[] args) {
-//         int choice;
-//         Scanner sc = new Scanner(System.in);
-//         Calculator calc = new Calculator();
-
-//         do {
-//             System.out.println("\nMENU");
-//             System.out.println("1. Addition");
-//             System.out.println("2. Subtraction");
-//             System.out.println("3. Multiplication");
-//             System.out.println("4. Division");
-//             System.out.println("5. Modulus");
-//             System.out.println("6. Exit");
-//             System.out.print("Enter your choice: ");
-
-//             choice = sc.nextInt();
-
-//             if (choice >= 1 && choice <= 5) {
-//                 System.out.print("Enter first number: ");
-//                 int x = sc.nextInt();
-//                 System.out.print("Enter second number: ");
-//                 int y = sc.nextInt();
-
-//                 switch (choice) {
-//                     case 1:
-//                         System.out.println("Sum = " + calc.add(x, y));
-//                         break;
-//                     case 2:
-//                         System.out.println("Difference = " + calc.sub(x, y));
-//                         break;
-//                     case 3:
-//                         System.out.println("Product = " + calc.mul(x, y));
-//                         break;
-//                     case 4:
-//                         System.out.println("Quotient = " + calc.div(x, y));
-//                         break;
-//                     case 5:
-//                         System.out.println("Remainder = " + calc.mod(x, y));
-//                         break;
-//                 }
-//             } 
-//             else if (choice == 6) {
-//                 System.out.println("Exiting...");
-//             } 
-//             else {
-//                 System.out.println("Invalid choice");
-//             }
-
-//         } while (choice != 6);
-
-//         sc.close();
-//     }
-// }
 import java.util.Scanner;
+
 class Calculator {
-    double add(double a, double b) {
+    int add(int a, int b) {
         return a + b;
     }
-    double sub(double a, double b) {
+
+    int sub(int a, int b) {
         return a - b;
     }
-    double mul(double a, double b) {
+
+    int mul(int a, int b) {
         return a * b;
     }
+
     double div(double a, double b) {
-        if (b == 0) {
-            System.out.println("Division by zero not allowed");
-            return 0;
-        }
         return a / b;
     }
+
     double mod(double a, double b) {
-        if (b == 0) {
-            System.out.println("Modulus by zero not allowed");
-            return 0;
-        }
         return a % b;
     }
 }
-public class CalculatorApp {
+
+class CalculatorApp {
     public static void main(String[] args) {
         int choice;
+        int x, y;
+        int result;
+        double res;
         Scanner sc = new Scanner(System.in);
         Calculator calc = new Calculator();
+
         do {
             System.out.println("\nMENU");
             System.out.println("1. Addition");
@@ -125,32 +41,68 @@ public class CalculatorApp {
             System.out.println("6. Exit");
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
-            if (choice >= 1 && choice <= 5) {
-                System.out.print("Enter first number: ");
-                double x = sc.nextDouble();
-                System.out.print("Enter second number: ");
-                double y = sc.nextDouble();
-                switch (choice) {
-                    case 1:
-                        System.out.println("Sum = " + calc.add(x, y));
-                        break;
-                    case 2:
-                        System.out.println("Difference = " + calc.sub(x, y));
-                        break;
-                    case 3:
-                        System.out.println("Product = " + calc.mul(x, y));
-                        break;
-                    case 4:
-                        System.out.println("Quotient = " + calc.div(x, y));
-                        break;
-                    case 5:
-                        System.out.println("Remainder = " + calc.mod(x, y));
-                        break;
-                }
-            } else if (choice == 6) {
-                System.out.println("Exiting...");
-            } else {
-                System.out.println("Invalid choice");
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter first number: ");
+                    x = sc.nextInt();
+                    System.out.print("Enter second number: ");
+                    y = sc.nextInt();
+                    result = calc.add(x, y);
+                    System.out.println("Sum = " + result);
+                    break;
+
+                case 2:
+                    System.out.print("Enter first number: ");
+                    x = sc.nextInt();
+                    System.out.print("Enter second number: ");
+                    y = sc.nextInt();
+                    result = calc.sub(x, y);
+                    System.out.println("Subtraction = " + result);
+                    break;
+
+                case 3:
+                    System.out.print("Enter first number: ");
+                    x = sc.nextInt();
+                    System.out.print("Enter second number: ");
+                    y = sc.nextInt();
+                    result = calc.mul(x, y);
+                    System.out.println("Multiplication = " + result);
+                    break;
+
+                case 4:
+                    System.out.print("Enter first number: ");
+                    double a = sc.nextDouble();
+                    System.out.print("Enter second number: ");
+                    double b = sc.nextDouble();
+                    if (b != 0) {
+                        res = calc.div(a, b);
+                        System.out.println("Division = " + res);
+                    }
+                    else
+                        System.out.println("Cannot divide by zero");
+                    break;
+
+                case 5:
+                    System.out.print("Enter first number: ");
+                    a = sc.nextDouble();
+                    System.out.print("Enter second number: ");
+                    b = sc.nextDouble();
+                    res = calc.mod(a, b);
+                    if (b != 0) {
+                        res = calc.mod(a, b);
+                        System.out.println("Modulus = " + res);
+                    }
+                    else
+                        System.out.println("Cannot modulus by zero");
+                    break;
+
+                case 6:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice");
             }
         } while (choice != 6);
         sc.close();
